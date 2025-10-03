2ca97806a52ff6af811f4084cd55bdd6663f0882
Dev <nameofdeev@gmail.com>
2025-10-03 21:19:35 +0545

    Added urls section in app and assigned project with apps

--- calculator_project/urls.py
+++ calculator_project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from calcul import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.calculator, name='calculator'),
+    path("", include("calcul.urls")),
 ]
